79f5ebb334ee50b807817a6b4a0e7d7f8370b520
cyl9 <cyl9@meitu.com>
2024-10-11 16:18:39 +0800

    完成4.3.3

--- Fahrzeug.cpp
+++ Fahrzeug.cpp
@@ -20,11 +20,36 @@ Fahrzeug::Fahrzeug(const std::string &name, double maxGeschwindigkeit)
             << " created, Max Speed: " << p_dMaxGeschwindigkeit << "km /h.\n ";
 }
 
-void Fahrzeug::vSimulieren() {
-  double zeitDifferenz = d_GlobaleZeit - p_dZeit;
-  if (zeitDifferenz > 0) {
-    p_dGesamtstrecke += zeitDifferenz * p_dMaxGeschwindigkeit;
-    p_dGesamtZeit += zeitDifferenz;
-    p_dZeit = d_GlobaleZeit;
-  }
+Fahrzeug::~Fahrzeug() {
+  std::cout << "Fahrzeug destroyed: Name=\"" << p_sName << "\", ID=" << p_iD
+            << std::endl;
 }
+
+void Fahrzeug::vKopf() {
+  std::cout << std::left << std::setw(5) << "ID" << std::setw(10) << "名称"
+            << std::setw(15) << "最大速度" << std::setw(10) << "总里程数"
+            << std::setw(10) << "Fuel" << std::endl;
+  std::cout << "--------------------------------------" << std::endl;
+}
+
+void Fahrzeug::vAusgeben() const {
+  std::cout << std::left << std::setw(5) << p_iD << std::setw(10) << p_sName
+            << std::setw(15) << std::fixed << std::setprecision(2)
+            << dGeschwindigkeit() << std::setw(10) << std::fixed
+            << std::setprecision(2) << p_dGesamtstrecke;
+}
+
+void Fahrzeug::vSimulieren() {
+  double dAktuelleGeschwindigkeit = dGeschwindigkeit();
+  double dZeitDelta = d_GlobaleZeit - p_dZeit;
+  if (dZeitDelta <= 0)
+    return;
+  double dGefahreneStrecke = dAktuelleGeschwindigkeit * dZeitDelta;
+
+  // 更新总行驶距离和总时间
+  p_dGesamtstrecke += dGefahreneStrecke;
+  p_dGesamtZeit += dZeitDelta;
+  p_dZeit = d_GlobaleZeit;
+}
+
+double Fahrzeug::dGeschwindigkeit() const { return p_dMaxGeschwindigkeit; }
--- Fahrzeug.h
+++ Fahrzeug.h
@@ -6,18 +6,23 @@
 #define FAHRZEUG_H
 #include <iomanip> // 用于格式化输出
 #include <iostream>
+#include <limits>
 #include <string>
 extern double d_GlobaleZeit;
 
 class Fahrzeug {
+
 private:
+  static int p_iMaxID; // 静态变量，确保每个车辆的ID唯一
+
+protected:
   std::string p_sName;          // 车辆名称
   const int p_iD;               // 车辆的唯一ID
-  static int p_iMaxID;          // 静态变量，确保每个车辆的ID唯一
   double p_dMaxGeschwindigkeit; // 最大速度
   double p_dGesamtstrecke;      // 总行驶距离
   double p_dGesamtZeit;         // 总行驶时间
   double p_dZeit;               // 最近一次模拟的时间
+
 public:
   Fahrzeug();
   explicit Fahrzeug(const std::string &name) : p_sName(name), p_iD(++p_iMaxID) {
@@ -25,37 +30,22 @@ public:
               << std::endl;
   }
   Fahrzeug(const std::string &name, double maxGeschwindigkeit);
+  virtual ~Fahrzeug();
 
-  ~Fahrzeug() {
-    std::cout << "Fahrzeug destroyed: Name=\"" << p_sName << "\", ID=" << p_iD
-              << std::endl;
-  }
+  static void vKopf();
 
-  // 输出表头函数
-  static void vKopf() {
-    std::cout << std::left << std::setw(5) << "ID" << std::setw(10) << "名称"
-              << std::setw(15) << "最大速度" << std::setw(10) << "总里程数"
-              << std::endl;
-    std::cout << "--------------------------------------" << std::endl;
-  }
+  virtual void vAusgeben() const;
 
-  // 输出车辆数据的函数
-  void vAusgeben() const {
-    std::cout << std::left << std::setw(5) << p_iD << std::setw(10) << p_sName
-              << std::setw(15) << std::fixed << std::setprecision(2)
-              << p_dMaxGeschwindigkeit << std::setw(10) << std::fixed
-              << std::setprecision(2) << p_dGesamtstrecke;
-  }
+  virtual void vSimulieren();
+  virtual double dGeschwindigkeit() const;
+  virtual double
+  dTanken(double dMenge = std::numeric_limits<double>::infinity()) {
+    return 0.0;
+  };
 
-  void vSimulieren();
-
-  // 返回车辆名称（常量成员函数）
   const std::string &getName() const { return p_sName; }
 
-  // 返回车辆ID（常量成员函数）
   int getID() const { return p_iD; }
 };
-
-// 静态变量初始化
 
 #endif // FAHRZEUG_H
